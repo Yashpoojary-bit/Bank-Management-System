5b2cad1f17bb63c4661a20c89e5992deac107420
Yashpoojary-bit <yashpoojary189@gmail.com>
2026-01-30 14:55:40 +0530

    Add files via upload

---
+++ src/bank/management/system/Conn.java
@@ -0,0 +1,20 @@
+package bank.management.system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+    Connection connection;
+    Statement statement;
+    public Conn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","Yash2005");
+            statement = connection.createStatement();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
